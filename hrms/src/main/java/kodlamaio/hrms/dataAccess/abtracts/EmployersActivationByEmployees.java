c1420bda117b365467be0e90740566f7a75b7088
esranurtat <79935867+esranurtat@users.noreply.github.com>
2021-06-10 15:38:43 +0300

    deneme

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abtracts/EmployersActivationByEmployees.java
+++
@@ -1,7 +0,0 @@
-package kodlamaio.hrms.dataAccess.abtracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//public interface EmployersActivationByEmployees  extends JpaRepository<EmployersActivationByEmployees, Integer>{
-
-//}
